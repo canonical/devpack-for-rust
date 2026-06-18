55d13a2550d8a914c0ce76a4d7548fd84771526d
Petrichor Park <petrichor.park@canonical.com>
2026-06-18 13:01:38 -0500

    original MVP without alias support

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,64 @@
 use console::Key;
+
+use eyre::bail;
 use treeversal::console_driver::{ConsoleDriver, Palette, TakeInput};
 
-mod make_tree;
 mod recipe;
+mod selection_tree;
 
-fn main() {
-    let tree = make_tree::make_tree();
-    let mut driver = ConsoleDriver::new_stdout(Palette::fancy(), tree);
-    driver.print_tree();
+/// Passed around for install parameters.
+#[derive(Default)]
+struct DevpackSettings {
+  /// If one recipe fails, choose whether to try and execute the other recipes.
+  pub continue_after_failure: bool,
+}
 
-    while let Ok(key) = driver.term.read_key() {
-        if key == Key::CtrlC {
-            break;
-        }
-        let res = driver.take_input(key);
-        if let Ok(TakeInput::Quit) = res {
-            break;
-        }
-        driver.print_tree();
+fn main() -> eyre::Result<()> {
+  let privileges = sudo::check();
+  if privileges != sudo::RunningAs::Root {
+    // the `sudo` crate supports restarting the program as root,
+    // but I would rather be explicit than implicit and require the user manually invoke sudo
+    bail!("devpack-for-rust must be run as root.");
+  }
+
+  let settings = DevpackSettings::default();
+
+  let tree = selection_tree::make_tree();
+  let mut driver = ConsoleDriver::new_stdout(Palette::fancy(), tree);
+  driver.print_tree();
+
+  while let Ok(key) = driver.term.read_key() {
+    if key == Key::CtrlC {
+      break;
     }
+    let res = driver.take_input(key);
+    if let Ok(TakeInput::Quit) = res {
+      break;
+    }
+    driver.print_tree();
+  }
 
-    let selected = driver.interactor.get_all_selected_data();
-    let selected_recipes = selected.iter().map(|smad| &smad.data).collect::<Vec<_>>();
-    println!("{:?}", &selected_recipes);
+  let selected = driver.interactor.get_all_selected_data();
+  let selected_recipes = selected.iter().map(|smad| &smad.data).collect::<Vec<_>>();
+
+  for recipe in selected_recipes.iter() {
+    'steps: for step in recipe.steps.iter() {
+      let res = step.execute(&settings);
+      if let Err(oh_no) = res {
+        if settings.continue_after_failure {
+          eprintln!("[devpack-for-rust] A recipe failed with the following error:");
+          eprintln!("{:?}", oh_no);
+          // the further steps of this recipe don't make sense,
+          // but nevertheless try to continue with the user's other demands
+          eprintln!("[devpack-for-rust] Continuing with further recipes as requested.");
+          break 'steps;
+        } else {
+          // quit!
+          return Err(oh_no);
+        }
+      }
+    }
+  }
+
+  Ok(())
 }
--- src/recipe.rs
+++ src/recipe.rs
@@ -1,35 +1,130 @@
+use std::process::Command;
+
+use eyre::{Context, bail};
+
+use crate::DevpackSettings;
+
 #[derive(Clone, Debug)]
 pub struct InstallRecipe {
-    pub steps: Vec<InstallStep>,
+  pub steps: Vec<InstallStep>,
 }
 
 impl InstallRecipe {
-    pub fn new(steps: Vec<InstallStep>) -> Self {
-        Self { steps }
-    }
+  pub fn new(steps: Vec<InstallStep>) -> Self {
+    Self { steps }
+  }
 
-    pub fn onestep(step: InstallStep) -> InstallRecipe {
-        Self::new(vec![step])
-    }
+  pub fn onestep(step: InstallStep) -> InstallRecipe {
+    Self::new(vec![step])
+  }
 
-    pub fn noop() -> InstallRecipe {
-        InstallRecipe::onestep(InstallStep::NoOp)
-    }
+  pub fn noop() -> InstallRecipe {
+    InstallRecipe::onestep(InstallStep::NoOp)
+  }
 }
 
 #[derive(Clone, Debug)]
 pub enum InstallStep {
-    /// For header nodes, etc
-    NoOp,
-    RustChannel(String),
-    Apt(String),
-    Snap {
-        package_name: String,
-        classic_confinement: bool,
-    },
-    /// `alias name=command`
-    MakeAlias {
-        name: String,
-        command: String,
-    }, // Command(Vec<String>),
+  /// For header nodes, etc
+  NoOp,
+  RustChannel(String),
+  Apt(String),
+  Snap {
+    package_name: String,
+    classic_confinement: bool,
+  },
+  /// `alias name=command`
+  MakeAlias {
+    name: String,
+    command: String,
+  }, // Command(Vec<String>),
+}
+
+impl InstallStep {
+  pub fn execute(&self, settings: &DevpackSettings) -> eyre::Result<()> {
+    match self {
+      InstallStep::NoOp => {
+        // that was easy
+        Ok(())
+      }
+      InstallStep::RustChannel(channel) => {
+        let rustup_path = match which::which("rustup") {
+          Ok(it) => it,
+          Err(_) => {
+            eprintln!("[devpack-for-rust] rustup was not found in path, installing via snap ...");
+            install_snap("rustup", true, settings)?;
+            which::which("rustup")
+              .wrap_err("even after `snap install`-ing rustup, could not find it in the path")?
+          }
+        };
+
+        println!(
+          "[devpack-for-rust] Using rustup to install rust channel {:?} ...",
+          &channel
+        );
+        let rustup_status = Command::new(rustup_path)
+          .arg("default")
+          .arg(&channel)
+          .status()?;
+        if !rustup_status.success() {
+          bail!("rustup invocation failed with error code {}", rustup_status);
+        }
+        Ok(())
+      }
+      InstallStep::Apt(pkg_name) => install_apt(pkg_name),
+      InstallStep::Snap {
+        package_name,
+        classic_confinement,
+      } => install_snap(package_name, *classic_confinement, settings),
+      InstallStep::MakeAlias { name, command } => todo!(),
+    }
+  }
+}
+
+fn install_apt(pkg_name: &String) -> Result<(), eyre::Error> {
+  println!("[devpack-for-rust] Using apt to install {:?} ...", pkg_name);
+  let apt_status = Command::new("apt").arg("update").status()?;
+  if !apt_status.success() {
+    bail!(
+      "apt update invocation failed with error code {}",
+      apt_status
+    );
+  }
+
+  let apt_status = Command::new("apt").arg("install").arg(pkg_name).status()?;
+  if !apt_status.success() {
+    bail!(
+      "apt install invocation failed with error code {}",
+      apt_status
+    );
+  }
+  Ok(())
+}
+
+fn install_snap(
+  package_name: &str,
+  classic_confinement: bool,
+  settings: &DevpackSettings,
+) -> eyre::Result<()> {
+  println!(
+    "[devpack-for-rust] using snap to install {:?}{}",
+    package_name,
+    if classic_confinement {
+      " with --classic confinement"
+    } else {
+      ""
+    }
+  );
+  // need to bind the cmd to a variable to appease borrowck
+  let mut cmd = Command::new("snap");
+  cmd.arg("install").arg(package_name);
+  if classic_confinement {
+    cmd.arg("--classic");
+  }
+
+  let status = cmd.status()?;
+  if !status.success() {
+    bail!("snap invocation failed with error code {}", status);
+  }
+  Ok(())
 }
